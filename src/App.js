ed785e571a95e73f4a48a62a6de3511027dd1200
abhishek <abhishekulagadde@gmail.com>
2024-01-21 23:27:16 +0530

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Notes from "./Notes";
+import Note from "./components/Note";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <Header />
+      {Notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
     </div>
   );
 }
